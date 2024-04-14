66c0d646261e0f10adb5396b32ee0a204c079494
ZhongxuYang <himatthew@foxmail.com>
2024-04-14 14:38:24 +0800

    fix: fix source Map warning on build (#11)

--- playground/vite-webapp/vite.config.ts
+++ playground/vite-webapp/vite.config.ts
@@ -12,6 +12,9 @@ const testPlugin: () => Plugin = () => ({
 })
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     vue(),
     vitePluginVersionMark({

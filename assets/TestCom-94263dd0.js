8a445bf573f1ef6778a0fb2fb11742842f74940a
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2024-08-13 03:19:51 +0000

    deploy: 38dd60813b53f802700acf6aa57e1af8a814b25a

--- assets/TestCom-94263dd0.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as c,c as t}from"./index-c45804b0.js";const o={};function n(r,s){return c(),t("div",null,"test com")}const a=e(o,[["render",n]]);export{a as default};
-//# sourceMappingURL=TestCom-94263dd0.js.map

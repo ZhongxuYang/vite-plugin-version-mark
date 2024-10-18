628809edaef35cf32a700c2a5cb314384e86d4be
peerless_hero <peerless_hero@outlook.com>
2024-10-18 23:00:38 +0800

    test: outputFile

--- __tests__/vite.test.ts
+++ __tests__/vite.test.ts
@@ -1,7 +1,8 @@
 import {resolve} from 'node:path'
+import {existsSync,readFileSync} from 'node:fs'
 import {describe, test, expect} from 'vitest'
-import {build} from 'vite'
-import type {RollupOutput, OutputAsset, OutputChunk} from 'rollup'
+import {build, type InlineConfig} from 'vite'
+import type {RollupOutput} from 'rollup'
 import {vitePluginVersionMark, type VitePluginVersionMarkInput} from '../src'
 
 const entryPathForVite = './fixtures/vite'
@@ -9,7 +10,7 @@ const entryFilenameForVite = 'index.html'
 const entryPathForLib = './fixtures/lib'
 const entryFilenameForLib = 'index.js'
 
-async function buildVite(pluginConfig: VitePluginVersionMarkInput, entryPath, entryFilename, buildOptions = {}) {
+async function buildVite(pluginConfig: VitePluginVersionMarkInput, entryPath: string, entryFilename: string, buildOptions: InlineConfig = {}) {
   const indexHtmlPath = resolve(__dirname, entryPath)
 
   const outputs = (await build({
@@ -23,11 +24,14 @@ async function buildVite(pluginConfig: VitePluginVersionMarkInput, entryPath, en
   const output = Array.isArray(outputs) ? outputs[0].output : outputs.output
   const file = output.find(
     item => item.fileName === entryFilename,
-  ) as OutputAsset | OutputChunk
-
-  const codeStr = file.source || file.code
-  
-  return await codeStr.toString()
+  )!
+  let codeStr = ''
+  if ('source' in file) {
+    codeStr = file.source.toString()
+  } else if ('code' in file) {
+    codeStr = file.code
+  }
+  return codeStr
 }
 
 // https://github.com/Applelo/unplugin-inject-preload/blob/main/test/vite.test.ts#L24
@@ -135,7 +139,78 @@ describe('VitePlugin', () => {
       )
       expect(output).toMatchSnapshot()
     })
+
+    test('output: File(true)', async () => {
+      const outDir = resolve(__dirname, entryPathForVite, 'dist')
+      await buildVite(
+        {
+          version: '1.0.0',
+          outputFile: true,
+          ifMeta: false,
+          ifLog: false,
+          ifGlobal: false,
+          ifExport: false,
+        },
+        entryPathForLib,
+        entryFilenameForLib,
+        {
+          build: {
+            lib: {
+              entry: './index.ts',
+              name: 'index',
+              fileName: 'index',
+              formats: ['es'],
+            },
+            outDir,
+          },
+        },
+      ) 
+      const filePath = resolve(outDir, '.well-known/version')
+      // File should be created
+      expect(existsSync(filePath)).toBe(true) 
+      // File should contain version
+      expect(readFileSync(filePath, 'utf-8')).toEqual('1.0.0')
+    })
+
+    test('output: File(custom function)', async () => {
+      const outDir = resolve(__dirname, entryPathForVite, 'dist')
+      const customFilePath = resolve(outDir, 'custom/version.json')
+      await buildVite(
+        {
+          version: '1.0.0',
+          outputFile(version){
+            return {
+              path: customFilePath,
+              content: `{"version":"${version}"}`,
+            }
+          },
+          ifMeta: false,
+          ifLog: false,
+          ifGlobal: false,
+          ifExport: false,
+        },
+        entryPathForLib,
+        entryFilenameForLib,
+        {
+          build: {
+            lib: {
+              entry: './index.ts',
+              name: 'index',
+              fileName: 'index',
+              formats: ['es'],
+            },
+            outDir,
+          },
+        },
+      ) 
+      // custom file should be created
+      expect(existsSync(customFilePath)).toBe(true) 
+      // custom file should contain version with json format
+      expect(readFileSync(customFilePath, 'utf-8')).toEqual(JSON.stringify({'version': '1.0.0'}))
+    })
   })
+
+
 
   // describe('longSHA', () => { })
   // describe('shortSHA', () => { })

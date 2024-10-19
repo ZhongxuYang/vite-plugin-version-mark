b01f2cd19c7fb7f11663dd897b602b3544dfdaf9
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2024-10-19 12:22:18 +0000

    deploy: f1537e94e458c4735c80186de4a772c277d320c8

--- assets/HelloWorld-306accd6.js
+++
@@ -1,2 +0,0 @@
-import{d as o,c as _,o as t,a as n,t as s,_ as l}from"./index-d113c0d5.js";const a=o({__name:"HelloWorld",setup(c){const e=_(()=>`Version: ${window.__VITE_VERSION__}`);return(r,d)=>(t(),n("h1",null,s(e.value),1))}});const i=l(a,[["__scopeId","data-v-7b243cd8"]]);export{i as default};
-//# sourceMappingURL=HelloWorld-306accd6.js.map
--- assets/TestCom-46bc6ff1.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as t,a as c}from"./index-d113c0d5.js";const o={};function n(r,s){return t(),c("div",null,"test com")}const _=e(o,[["render",n]]);export{_ as default};
-//# sourceMappingURL=TestCom-46bc6ff1.js.map

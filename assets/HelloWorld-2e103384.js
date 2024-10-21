c5455fc47f9b69bce5e8fef0558267d78e1ed04e
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2024-10-21 03:28:23 +0000

    deploy: a6b420cba9565c3e19cb23c8a59d95dfc052e89d

---
+++ assets/HelloWorld-2e103384.js
@@ -0,0 +1,2 @@
+import{d as o,c as _,o as t,a as n,t as s,_ as l}from"./index-c4dc4a17.js";const a=o({__name:"HelloWorld",setup(c){const e=_(()=>`Version: ${window.__VITE_VERSION__}`);return(r,d)=>(t(),n("h1",null,s(e.value),1))}});const i=l(a,[["__scopeId","data-v-7b243cd8"]]);export{i as default};
+//# sourceMappingURL=HelloWorld-2e103384.js.map

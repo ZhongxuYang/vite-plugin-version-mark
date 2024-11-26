0ea902020ed849cedac5575846d0475c17cca307
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2024-11-26 15:52:12 +0000

    deploy: 195e4bf7e715808bb1243f25bda360b674f0ba37

--- assets/TestCom-8ac8e5fe.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as t,a as c}from"./index-de2e5307.js";const o={};function n(r,s){return t(),c("div",null,"test com")}const _=e(o,[["render",n]]);export{_ as default};
-//# sourceMappingURL=TestCom-8ac8e5fe.js.map
---
+++ assets/TestCom-a4f95753.js
@@ -0,0 +1,2 @@
+import{_ as e,o as t,a as c}from"./index-678d2bb4.js";const o={};function n(r,s){return t(),c("div",null,"test com")}const _=e(o,[["render",n]]);export{_ as default};
+//# sourceMappingURL=TestCom-a4f95753.js.map

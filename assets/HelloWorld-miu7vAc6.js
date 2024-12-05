422e11050cd8b48e2ade3038bb8c1607de081359
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2024-12-05 05:08:00 +0000

    deploy: 908afb7b4a2deb478b15053654e32668ba326966

---
+++ assets/HelloWorld-miu7vAc6.js
@@ -0,0 +1,2 @@
+import{d as e,c as t,o as n,a as s,t as _,_ as a}from"./index-CTNZ85mn.js";const c=e({__name:"HelloWorld",setup(r){const o=t(()=>`Version: ${window.__VITE_VERSION__}`);return(l,p)=>(n(),s("h1",null,_(o.value),1))}}),i=a(c,[["__scopeId","data-v-7b243cd8"]]);export{i as default};
+//# sourceMappingURL=HelloWorld-miu7vAc6.js.map
--- assets/TestCom-D7tK0cdq.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,a as t,o as c}from"./index-CRLxllJp.js";const o={};function n(r,s){return c(),t("div",null,"test com")}const _=e(o,[["render",n]]);export{_ as default};
-//# sourceMappingURL=TestCom-D7tK0cdq.js.map

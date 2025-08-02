f5cfecb9f39e525b71f0c87c62305345ae11e816
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2025-08-02 12:54:13 +0000

    deploy: f4977e03032c6330d1f3783e50f616eb59d1286e

---
+++ assets/HelloWorld-Dkhn_T_B.js
@@ -0,0 +1,2 @@
+import{d as e,c as t,a as n,t as s,o as _,_ as a}from"./index-D5cIPhAf.js";const c=e({__name:"HelloWorld",setup(r){const o=t(()=>`Version: ${window.__VITE_VERSION__}`);return(l,p)=>(_(),n("h1",null,s(o.value),1))}}),i=a(c,[["__scopeId","data-v-7b243cd8"]]);export{i as default};
+//# sourceMappingURL=HelloWorld-Dkhn_T_B.js.map
--- assets/TestCom-CTQamdbR.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,a as t,o as c}from"./index-CTNZ85mn.js";const o={};function n(r,s){return c(),t("div",null,"test com")}const _=e(o,[["render",n]]);export{_ as default};
-//# sourceMappingURL=TestCom-CTQamdbR.js.map

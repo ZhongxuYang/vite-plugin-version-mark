e6a3e7a0c0f8722d82dd385f09c64e4baa7c5f0a
ZhongxuYang <ZhongxuYang@users.noreply.github.com>
2026-09-07 09:33:05 +0000

    deploy: 6dfd3fc655e971ee3593c15b8b0ef04d6f19a900

---
+++ assets/HelloWorld-CSmNVimg.js
@@ -0,0 +1,2 @@
+import{d as e,c as t,t as _,a,o as n,_ as s}from"./index-DusjwU4s.js";const r=e({__name:"HelloWorld",setup(c){const o=a(()=>`Version: ${window.__VITE_WEBAPP_VERSION__}`);return(l,p)=>(n(),t("h1",null,_(o.value),1))}}),i=s(r,[["__scopeId","data-v-a716d316"]]);export{i as default};
+//# sourceMappingURL=HelloWorld-CSmNVimg.js.map
---
+++ assets/TestCom-DQQ5krhW.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o as t}from"./index-DusjwU4s.js";const o={};function n(r,s){return t(),c("div",null,"test com")}const a=e(o,[["render",n]]);export{a as default};
+//# sourceMappingURL=TestCom-DQQ5krhW.js.map
